4471287edf1fb3f7d5f9606b3213af162d067fc8
JonasMalik <jonas14550@gmail.com>
2016-01-13 15:02:29 +0100

    added MVC and Player class

---
+++ src/com/company/Model/Player.java
@@ -0,0 +1,16 @@
+package com.company.Model;
+
+/**
+ * Created by jonas on 2016-01-13.
+ */
+public class Player {
+
+    String name;
+    String piece;
+    int wins;
+
+    public Player(String name, String piece){
+        this.name = name;
+        this.piece = piece;
+    }
+}
